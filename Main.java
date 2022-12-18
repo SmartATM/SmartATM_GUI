7d868e913989ae6e53740432f89ceed5d1f584c3
sh-532 <101563555+sh-532@users.noreply.github.com>
2022-12-18 17:34:22 +0900

    Add files via upload

--- Main.java
+++ Main.java
@@ -4,22 +4,24 @@ package org.example;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
         System.out.println("Hello 안녕 world!");
 
-        gui GUI = new gui();
+        default_gui homeGui = new default_gui();
+
+        //GUI.revalidate();
+        //GUI.repaint();
+
+
 
         Scanner scan = new Scanner(System.in);
         String s = scan.next();
 
         if(s.equals("hello")) {
-            GUI.changeInputAmout("30000");
+            //GUI.changeInputAmout("30000");
         } else if (s.equals("replace")) {
             //GUI.changeUIScreen(3);
         }
     }
-
-
-
-
 }
